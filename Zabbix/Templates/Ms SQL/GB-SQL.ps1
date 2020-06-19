a4a56d2567450ea33a7388192cbd15fbfb962194
Ezequiel Godoy <egodoy@genbyte.com.ar>
2020-06-18 21:18:24 -0300

    update & doc

--- Zabbix/Templates/Ms SQL/GB-SQL.ps1
+++ Zabbix/Templates/Ms SQL/GB-SQL.ps1
@@ -1,45 +1,119 @@
+<#
+.SYNOPSIS
+Discover Services, DB Names, or get sizes
+
+.DESCRIPTION
+This script is intended to be used in Zabbix Agent for Windows, and will return single values or JSON structures. 
+It can be used to discover the SQL Services and Databases, or get the DB (Log and Data files) sizes.
+You will also find relevant information like free space in files, and growth
+You will need a local user in SQL with the appropriate permissions.
+
+.PARAMETER Operation
+Operations allowed:
+ + services: Discover all SQL instances (and it's related service)
+ + discover: Discover all DB names and it's corresponding ID
+ + sizeDAT: Get DB Data file/s Size. You will need to provide DB Name
+ + sizeLOG: Get DB Log file/s Size. You will need to provide DB Name
+ + freeDAT: Get free space in DATA File
+ + freeLOG: Get free space in LOG File
+ + grDAT: Get how much the DB DATA file will grogth nex time
+ + grLOG: Get how much the DB LOG file will grogth nex time
+ + grpDAT: If the DATA file growth in %
+ + grpLOG: If the LOG file growth in %
+.PARAMETER UsrSQL
+Local user in SQL Server
+.PARAMETER PwdSQL
+Password for local user in SQL Server
+.PARAMETER DBname
+Used to identify the DB for "size operation"
+
+.EXAMPLE
+PS> GB-SQL.ps1 -Operation discover -UsrSQL "Usr" -PwdSQL "Pwd"
+{"data":
+[
+    {
+        "{#NAME}":  "DB_NAME_1",
+        "{#DBID}":  5
+    },
+    {
+        "{#NAME}":  "DB_NAME_2",
+        "{#DBID}":  6
+    }
+]
+}
+
+Discover all databases  (return Name and ID) exept system
+
+.EXAMPLE
+PS> GB-SQL.ps1 -Operation services
+{"data":
+{
+    "#DNAME":  "SQL Server (MSSQLSERVER)",
+    "#NAME":  "MSSQLSERVER"
+}
+}
+
+Discover all instances and corresponding services
+
+.EXAMPLE
+PS> GB-SQL.ps1 -Operation sizeDAT -UsrSQL "Usr" -PwdSQL "Pwd" -DBname "DadaBaseName"
+321088
+
+Get DB (in this case with ID 5) DATA size in MB
+
+#>
+
 Param(
-    [string]$UsrSQL,
-    [string]$PwdSQL,
+    [Parameter(Position=0, Mandatory=$true)]
+    [ValidateSet("services","discover","sizeDAT","sizeLOG","freeDAT","freeLOG","grDAT","grLOG","grpDAT","grpLOG")]
     [string]$Operation,
-    [string]$DBid
+
+    [Parameter(Position=1, Mandatory=$false)]
+    [string]$UsrSQL,
+
+    [Parameter(Position=2, Mandatory=$false)]
+    [string]$PwdSQL,
+
+    [Parameter(Position=3, Mandatory=$false)]
+    [string]$DBname
     )
 
-$sqlServer = $env:COMPUTERNAME
-$connectionString = "Server = $sqlServer; User ID = $UsrSQL; Password = $PwdSQL;"
-#$connectionString = "Server = $sqlServer; Integrated Security = True;"
+function OuputZabbix {
 
-$connection = New-Object System.Data.SqlClient.SqlConnection
-$connection.ConnectionString = $connectionString
-$connection.Open()
-
-if ($Operation -eq "discover"){
-    
-    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
-    $SqlCmd.CommandText = 'SELECT name as "{#NAME}",dbid as "{#DBID}" FROM sysdatabases WHERE dbid > 4'
-    $SqlCmd.Connection = $Connection
-    $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
-    $SqlAdapter.SelectCommand = $SqlCmd
-    $DataSet = New-Object System.Data.DataSet
-    $SqlAdapter.Fill($DataSet) > $null
-
-    $baseName = $DataSet.Tables[0]
+    param (
+        [array]$data
+    )
     
     Write-Host "{`"data`":"
-    Write-Host (ConvertTo-Json ($baseName | Select-Object "{#NAME}","{#DBID}"))
+    Write-Host (ConvertTo-Json $data)
     Write-Host "}"
+}
 
-    }
+function ConnectSQL {
+    param (
+        [string]$UsrSQL,
+        [string]$PwdSQL
+    )
 
-if ($Operation.StartsWith("size")){
+    $connection = New-Object System.Data.SqlClient.SqlConnection
     
-    $query =  "SELECT "
-	$query += "ROUND(SUM(mf.size) * 8, 0) AS `'SIZEMB`' "
-    $query += "FROM sys.master_files mf "
-    $query += "WHERE database_id = $DBid"
-    if ($Operation -eq "sizeDAT") {$query += "AND type_desc = `'ROWS`'"}
-    else {$query += "AND type_desc = `'LOG`'"}
+    $sqlServer = $env:COMPUTERNAME
+    $connectionString = "Server = $sqlServer; User ID = $UsrSQL; Password = $PwdSQL;"
+    #$connectionString = "Server = $sqlServer; Integrated Security = True;"
 
+    $connection.ConnectionString = $connectionString
+
+    return $connection
+}
+
+function querySQL {
+    param (
+        $connection,
+        $query
+    )
+    
+    $connection.Open()
+        
     $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
     $SqlCmd.CommandText = $query
     $SqlCmd.Connection = $Connection
@@ -48,10 +122,130 @@ if ($Operation.StartsWith("size")){
     $DataSet = New-Object System.Data.DataSet
     $SqlAdapter.Fill($DataSet) > $null
 
-    $baseSize = $DataSet.Tables[0]
-    
-    Write-Host ($baseSize.SIZEMB)
+    $Connection.Close()
 
+    return $DataSet
+}
+
+function buildSizeQuery {
+    param (
+        [string]$fileTipe,
+        [string]$DBname
+    )
+
+    $query = @( "USE DB-NAME"
+                "SELECT size/128 AS `'SIZEMB`'",
+                ",size/128 - CAST(FILEPROPERTY(name, 'SpaceUsed') AS INT)/128 AS `'FREESIZEMB`'",
+                ",growth AS `'FILEGROWTH`'",
+                ",is_percent_growth AS `'FILEGROWTHPERCENT`'",
+                "FROM sys.database_files WHERE type IN (0,1)",
+                "AND type_desc = `'DB-FILE`'"
+                )
+
+    $query = $query -replace "DB-NAME","$DBname"
+    $query = $query -replace "DB-FILE","$fileTipe"
+
+    return $query
+}
+
+switch ($Operation) {
+    "services" {  
+
+        $sqlInstances = get-service | Where-Object {$_.DisplayName -like "*SQL Server (*"} | `
+        Select-Object -Property @{label='{#DNAME}';expression={$_.DisplayName}},@{label='{#NAME}';expression={$_.Name}}
+    
+        OuputZabbix $sqlInstances
     }
 
-$Connection.Close()
+    "discover" {
+
+        $query = 'SELECT name as "{#NAME}",dbid as "{#DBID}" FROM sysdatabases WHERE dbid > 4'
+        $dataSet = querySQL (ConnectSQL $UsrSQL $PwdSQL) $query
+    
+        $baseName = $DataSet.Tables[0]
+        
+        OuputZabbix ($baseName | Select-Object "{#NAME}","{#DBID}")
+    }
+
+    "sizeLOG" {
+
+        $query = buildSizeQuery "LOG" $DBname
+        $dataSet = querySQL (ConnectSQL $UsrSQL $PwdSQL) $query
+
+        $baseSize = $DataSet.Tables[0]
+    
+        Write-Host ($baseSize.SIZEMB)
+    }
+
+    "sizeDAT" {
+
+        $query = buildSizeQuery "ROWS" $DBname
+        $dataSet = querySQL (ConnectSQL $UsrSQL $PwdSQL) $query
+
+        $baseSize = $DataSet.Tables[0]
+    
+        Write-Host ($baseSize.SIZEMB)
+    }
+
+    "freeDAT" {
+
+        $query = buildSizeQuery "ROWS" $DBname
+        $dataSet = querySQL (ConnectSQL $UsrSQL $PwdSQL) $query
+
+        $baseSize = $DataSet.Tables[0]
+    
+        Write-Host ($baseSize.FREESIZEMB)
+    }
+    
+    "freeLOG" {
+
+        $query = buildSizeQuery "LOG" $DBname
+        $dataSet = querySQL (ConnectSQL $UsrSQL $PwdSQL) $query
+
+        $baseSize = $DataSet.Tables[0]
+    
+        Write-Host ($baseSize.FREESIZEMB)
+    }
+    
+    "grDAT" {
+
+        $query = buildSizeQuery "ROWS" $DBname
+        $dataSet = querySQL (ConnectSQL $UsrSQL $PwdSQL) $query
+
+        $baseSize = $DataSet.Tables[0]
+    
+        Write-Host ($baseSize.FILEGROWTH)
+    }
+
+    "grLOG" {
+
+        $query = buildSizeQuery "LOG" $DBname
+        $dataSet = querySQL (ConnectSQL $UsrSQL $PwdSQL) $query
+
+        $baseSize = $DataSet.Tables[0]
+    
+        Write-Host ($baseSize.FILEGROWTH)
+    }
+    
+    "grpDAT" {
+
+        $query = buildSizeQuery "ROWS" $DBname
+        $dataSet = querySQL (ConnectSQL $UsrSQL $PwdSQL) $query
+
+        $baseSize = $DataSet.Tables[0]
+    
+        Write-Host ($baseSize.FILEGROWTHPERCENT)
+    }
+
+    "grpLOG" {
+
+        $query = buildSizeQuery "LOG" $DBname
+        $dataSet = querySQL (ConnectSQL $UsrSQL $PwdSQL) $query
+
+        $baseSize = $DataSet.Tables[0]
+    
+        Write-Host ($baseSize.FILEGROWTHPERCENT)
+    }
+
+    Default {}
+}
---
+++ Zabbix/Templates/Ms SQL/README.md
@@ -0,0 +1,73 @@
+
+# Ms SQL Template for Zabbix
+
+This template was designed for:
+
+* Discover all SQL Instances, and corresponding windows service
+* Discover all databases (except system DB) and return names and ID
+* Query DB Data and Log file size
+* Query DB Data and Log free space on files
+* Query DB Data and Log file growht conifguration
+* Query performance counters related to SQL
+
+## Requeriments in SQL Server
+
+You will need to create a local user with the correct privileges, and update the corresponding macros in template:
+
+* {$USR}: Local SQL user
+* {$PWD}: local SQL user password
+
+With this parameters set you will be able to discover all DB and query it's sizes.
+
+## Service
+
+The most used conifugration in SQL is to have only one instance and service in the Server. Owever, we can find other situations.
+
+This template will search and register all instances and related services.
+
+:rotating_light: *Triggers an alert when service is stopped*
+
+## Performance Counters
+
+### Access Methods\Page Splits/sec
+
+> "\SQLServer:Access Methods\Page Splits/sec"
+
+For those not familiar with page splits, let’s first take a look at what they are. Whenever you INSERT new data into a SQL Server table, or in some cases, UPDATE data in a SQL Server table, there may not enough room for the newly INSERTed or UPDATEd row in the applicable data page. In this case, SQL Server will have to move some of the data from the current data page and move it to a new data page. So not only does SQL Server experience the normal I/O of INSERTing or UPDATing a row, it must also move data and update any applicable indexes. This can result in a lot of excess I/O.
+
+Ref: <https://www.sql-server-performance.com/reduce-page-splits/>
+
+### Buffer cache hit ratio
+
+> "\SQLServer:Buffer Manager\Buffer cache hit ratio"
+
+Buffer Cache Hit Ratio shows how SQL Server utilizes buffer cache.
+It gives the ratio of the data pages found and read from the SQL Server buffer cache and all data page requests. The pages that are not found in the buffer cache are read from the disk, which is significantly slower and affects performance.
+Ideally, SQL Server would read all pages from the buffer cache and there will be no need to read any from disk. In this case, the Buffer Cache Hit Ratio value would be 100. The recommended value for Buffer Cache Hit Ratio is over 90.
+
+:rotating_light: *Triggers an alert when value is below 90*
+
+Ref: <https://www.sqlshack.com/sql-server-memory-performance-metrics-part-4-buffer-cache-hit-ratio-page-life-expectancy/>
+
+### Page life expectancy
+
+> "\SQLServer:Buffer Manager\Page life expectancy"
+
+SQL Server has more chances to find the pages in the buffer pool if they stay there longer. If the page is not in the buffer pool, it will be read from disk, which affects performance. If there’s insufficient memory, data pages are flushed from buffer cache more frequently, to free up the space for the new pages.
+When there’s sufficient memory on the server, pages have a high life expectancy. The normal values are above 300 seconds (5 minutes) and the trend line should be stable. It’s recommended to monitor the values over time, as frequent quick drops indicate memory issues. Also, a value drop of more than 50% is a sign for deeper investigation.
+
+Ref: <https://www.sqlshack.com/sql-server-memory-performance-metrics-part-4-buffer-cache-hit-ratio-page-life-expectancy/>
+
+### Processes blocked
+
+> "\SQLServer:General Statistics\Processes blocked"
+
+In any multi-user application you're going to have blocked processes, and SQL Server has mechanisms to handle blocked processes well, but when this counter goes outside the normal range (for your system) you'll want to investigate and see what might be causing the issue. There could be excessive blocking due to page escalation, for example, where entire tables are getting locked instead of individual rows or pages.
+
+### SQL Compilations (and Re-Compilations)
+
+> "\SQLServer:SQL Statistics\SQL Compilations/sec"
+>
+> "\SQLServer:SQL Statistics\SQL Re-Compilations/sec"
+
+These counters will increment when SQL Server has to compile or recompile query plans because either the plan in cache is no longer valid, or there's no plan in cache for this query. SQL Server uses a cost-based optimizer that relies on statistics to choose a good query plan, and when those statistics are out-of-date, additional compilations are done unnecessarily. It can be useful to understand the source of this problem, if it is a problem (this might be expected behavior, depending on the workload).
